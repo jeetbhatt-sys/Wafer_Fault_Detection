987d6fa98346d4e12c86eab040c6746ff05d75af
Bhatt <jitender.bhatt@nokia.com>
2021-11-22 22:19:08 +0530

    new commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-heroku",
+    version="0.0.3",
+    description="ML project",
+    author="Jeet Bhatt",
+    packages=find_packages(),
+    license="MIT"
+)
